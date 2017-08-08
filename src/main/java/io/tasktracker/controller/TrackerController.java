9587baf3ab9bc7e9ca13c27f99cdfb6210a5dc18
Paul Dev <paul_dev@tutanota.com>
2017-08-07 22:50:59 -0400

    added some more files

--- src/main/java/io/tasktracker/controller/TrackerController.java
+++ src/main/java/io/tasktracker/controller/TrackerController.java
@@ -6,26 +6,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class LoginController {
+public class TrackerController {
 	String message = "Welcome to Spring MVC!";
- 
-	@RequestMapping("/login.do")
+
+	@RequestMapping("tracker/landing.do")
 	public ModelAndView showMessage(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		System.out.println("in controller");
- 
-		ModelAndView mv = new ModelAndView("login");
+		System.out.println("in TrackerController");
+
+		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("message", message);
 		mv.addObject("name", name);
-		return mv;
-	}
-	
-
-	
-	@RequestMapping("/")
-	public ModelAndView login() {
-		System.out.println("before login"); 
-		ModelAndView mv = new ModelAndView("login");
 		return mv;
 	}
 }
